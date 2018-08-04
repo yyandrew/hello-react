39e015215194ed4842d059f292c9a0089684b177
andrew yang <qqerqqer@126.com>
2018-08-04 17:40:24 +0800

    Component

---
+++ src/App.js
@@ -0,0 +1,64 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      data: [
+        {
+          "id":1,
+          "name":"Foo",
+          "age":"20"
+        },
+        {
+          "id":2,
+          "name":"Bar",
+          "age":"30"
+        },
+        {
+          "id":3,
+          "name":"Baz",
+          "age":"40"
+        }
+      ]
+    }
+  }
+  render() {
+    return (
+      <div>
+        <Header />
+        <table>
+          <tbody>
+            { this.state.data.map((person, i) => <TableRow key={i} data={person} />) }
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
+
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Header</h1>
+      </div>
+    )
+  }
+}
+
+class TableRow extends Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.data.id}</td>
+        <td>{this.props.data.name}</td>
+        <td>{this.props.data.age}</td>
+      </tr>
+    )
+  }
+
+}
+export default App;
