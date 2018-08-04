1c01ba68124ba41b4f888000f0df752c93aa8ca0
andrew yang <qqerqqer@126.com>
2018-08-04 17:57:49 +0800

    Props Validation - isRequired: If props has no such props there will be a error

--- src/App.js
+++ src/App.js
@@ -1,45 +1,38 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      header: "Header from state...",
-      content: "Content from stats...",
-    }
-  }
   render() {
     return (
       <div>
-        <h1>{this.state.header}</h1>
-        <h2>{this.state.content}</h2>
+        <h1>Hello, {this.props.name}</h1>
+        <h3>Array: {this.props.propArray}</h3>
+        <h3>Bool: {this.props.propBool ? "True..." : "False..."}</h3>
+        <h3>Func: {this.props.propFunc(3)}</h3>
+        <h3>Number: {this.props.propNumber}</h3>
+        <h3>String: {this.props.propString}</h3>
       </div>
     );
   }
 }
-
-class Header extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Header</h1>
-      </div>
-    )
-  }
+App.propTypes = {
+  name: PropTypes.string,
+  propArray: PropTypes.array.isRequired,
+  propBool: PropTypes.bool.isRequired,
+  propFunc: PropTypes.func,
+  propNumber: PropTypes.number,
+  propString: PropTypes.string,
 }
-
-class TableRow extends Component {
-  render() {
-    return (
-      <tr>
-        <td>{this.props.data.id}</td>
-        <td>{this.props.data.name}</td>
-        <td>{this.props.data.age}</td>
-      </tr>
-    )
-  }
-
+App.defaultProps = {
+  name: "Get start with ReactJS",
+  propArray: [1, 2, 3, 4, 5],
+  propBool: false,
+  propFunc: function(e) {
+    return e;
+  },
+  propNumber: 1,
+  propString: "String Value...",
 }
 export default App;
