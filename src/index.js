49c57f5b796b4c087dd3d1090090e9e8d872fefc
andrew yang <qqerqqer@126.com>
2018-11-10 22:15:59 +0800

    MISC - Add redux and react-redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
 import './index.css'
 import AppRouter from './route'
+import store from './store'
 import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<AppRouter />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>,
+  document.getElementById('root'))
 registerServiceWorker()
