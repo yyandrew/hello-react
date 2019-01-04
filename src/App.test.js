169a07aa3e28ba4ae857342affa5a0dd65ef63ee
andrew yang <qqerqqer@126.com>
2019-01-04 18:26:57 +0800

    MISC - Config enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { shallow } from 'enzyme';
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
 });
+
+it('render a label', () => {
+  const wrapper = shallow(
+    <label>andrew</label>
+  )
+  expect(wrapper).toMatchSnapshot()
+})
